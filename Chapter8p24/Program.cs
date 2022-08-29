9645d1c9a2db5058e86d2fc621878d4e5c750106
gidits13 <gidits13@gmail.com>
2022-08-29 10:00:00 +0300

    Добавьте файлы проекта.

---
+++ Chapter8p24/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Chapter8p24
+{
+    internal class Program
+    {
+        static void MoveToRBin()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"/Users/VARGGOTH/Desktop/testFolder");
+                string trashPath = "/$Recycle.Bin/testFolder";
+
+                dirInfo.MoveTo(trashPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            MoveToRBin();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
